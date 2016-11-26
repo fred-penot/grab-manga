ee81424fc4976d090347d5059da946f58c7ac549
fred-penot <frederic.penot@gmail.com>
2016-11-26 23:11:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ cd /home/${login_ssh}/GrabManga
 sh install.sh
 chmod -Rf 777 /home/${login_ssh}/GrabManga
 chown -R ${login_ssh}:${login_ssh} /home/${login_ssh}/GrabManga
+
+mkdir /root/.ionic
+echo '{' > /root/.ionic/ionic.config
+echo '  "ionicServeAddress": "172.17.0.2"' >> /root/.ionic/ionic.config
+echo '}' >> /root/.ionic/ionic.config
+chmod -Rf 755 /root/.ionic
